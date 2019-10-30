343b6278a502bd34ff51aa509dcccc452ee81bb9
lakshmi kalavathi <kalareddy.a@gmail.com>
2019-10-30 16:20:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var errors = new List<string>();
+
+            
+
+        }
+    }
+}
---
+++ UserErrorclass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Polymorphism
+{
+    public abstract class UserErrorClass   // Abstract class
+    {
+        public abstract string UEMessage();  // Abstract method
+
+    }
+
+
+    public class NumericInputError : UserErrorClass
+    {
+
+        public override string UEMessage()
+        {
+            string str = "You tried to use numeric input in a text only field. This fired an error!";
+          return str;
+        }
+
+    }
+
+
+    public class TextInputError : UserErrorClass
+    {
+
+        public override string UEMessage()
+        {
+            string str = "You tried to use a textinput in a numericonly field. This fired an error!";
+            return str;
+        }
+
+    }
+
+}
